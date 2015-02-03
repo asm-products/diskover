9af9285dcee5b25bd6c0dc4ce2311897e604a8c3
luduvigo <paoloantoniorossi@gmail.com>
2015-02-03 13:55:37 +0000

    Change server.js to previous version

--- app/routes/user_route.js
+++ app/routes/user_route.js
@@ -1,47 +1,26 @@
 // Fake user database
 
-var users = [
-  { name: 'TJ', email: 'tj@vision-media.ca' },
-  { name: 'Tobi', email: 'tobi@vision-media.ca' }
-];
+console.log("User extracted");
 
 exports.list = function(req, res){
-  res.render('users', { title: 'Users', users: users });
+  console.log("User LISTED");
+  return 0;
 };
 
 exports.load = function(req, res, next){
-  var id = req.params.id;
-  req.user = users[id];
-  if (req.user) {
-    next();
-  } else {
-    var err = new Error('cannot find user ' + id);
-    err.status = 404;
-    next(err);
-  }
+    console.log("User LOADED");
 };
 
 exports.view = function(req, res){
-  res.render('users/view', {
-    title: 'Viewing user ' + req.user.name,
-    user: req.user
-  });
+  console.log("User VIEWED");
 };
 
 exports.edit = function(req, res){
-  res.render('users/edit', {
-    title: 'Editing user ' + req.user.name,
-    user: req.user
-  });
+    console.log("User EDITED");
 };
 
 exports.update = function(req, res){
-  // Normally you would handle all kinds of
-  // validation and save back to the db
-  var user = req.body.user;
-  req.user.name = user.name;
-  req.user.email = user.email;
-  res.redirect('back');
+  console.log("User UPDATED");
 };
 
 
--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+
+
+// //Routes for our API 
+// // ==========================================================================
+// var router = express.Router();
+
+// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// router.get('/', function(req, res) {
+//     res.json({ message: 'hooray! welcome to our api!' });   
+// });
+
+// // mapping the routes
+// var bears = require('./app/routes/bear_route');
+// var users = require('./app/routes/user_route');
+
+// // REGISTER OUR ROUTES -------------------------------
+// // all of our routes will be prefixed with /api
+// //app.use('/api/bears', bears);
+// app.use('/api/users', users);
+
+// //should follow this example:
+// //https://github.com/strongloop/express/blob/master/examples/route-separation/index.js
+
+// // User
+
+// app.get('/users', user.list);
+
+// //app.get('/api/user/list', users.list);
+
+
+///////////////////////////////////////////////////////////////////////////
+
 //server.js
 
 //Base setup 
@@ -23,40 +55,100 @@ var Bear        = require('./app/models/bear');
 // ==========================================================================
 var router = express.Router();
 
+//middleware to use for all the requests
+router.use(function(req, res, next) {
+  //do logging
+  console.log('- API request received');
+  next(); //make sure we go to the next routes and we do not stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// mapping the routes
-var bears = require('./app/routes/bear_route');
-var users = require('./app/routes/user_route');
+// more routes for our API will happen here
+
+// on routes that end in /bears
+// ===========================================================================
+router.route('/bears')
+  // create a bear (accessed at POST http://localhost:8080/api/bears)
+  .post(function(req, res) {
+  
+    var bear = new Bear();      // create a new instance of the bear model
+    bear.name = req.body.name;  // set the bears name (comes from the request)
+    
+    console.log('INSERT: ' + req.body.name);
+  
+    // save the bear and check for errors
+    bear.save(function(err){
+      if(err)
+          res.send(err);
+      
+      res.json({ message: 'Bear created!'});
+    });
+  })
+
+  .get(function(req, res) {
+    Bear.find(function(err, bears){
+      if (err)
+        res.send(err);
+      
+      res.json(bears);
+    });
+  });
+
+// on routes that end in /bears/:bear_id
+// ----------------------------------------------------
+router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+    
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    })
+    
+    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-//app.use('/api/bears', bears);
-//app.use('/api/users', users);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-//should follow this example:
-//https://github.com/strongloop/express/blob/master/examples/route-separation/index.js
-
-// User
-
-//app.get('/users', user.list);
-// app.all('/user/:id/:op?', user.load);
-// app.get('/user/:id', user.view);
-// app.get('/user/:id/view', user.view);
-// app.get('/user/:id/edit', user.edit);
-// app.put('/user/:id/edit', user.update);
-
-//app.get('/api/user/list', users.list);
+app.use('/api', router);
 
 // START THE SERVER
 // ===========================================================================
